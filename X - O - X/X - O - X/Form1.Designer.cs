7962c342aeeb09012706145f526827a454f95ac8
Erol Dalkılıç <64806208+eroldalk@users.noreply.github.com>
2022-02-09 22:18:47 +0300

    Add files via upload

---
+++ X - O - X/X - O - X/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace X___O___X
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b1.Location = new System.Drawing.Point(12, 12);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 100);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b3.Location = new System.Drawing.Point(224, 12);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 100);
+            this.b3.TabIndex = 1;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b2.Location = new System.Drawing.Point(118, 12);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 100);
+            this.b2.TabIndex = 2;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b5.Location = new System.Drawing.Point(118, 118);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(100, 100);
+            this.b5.TabIndex = 5;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b6.Location = new System.Drawing.Point(224, 118);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(100, 100);
+            this.b6.TabIndex = 4;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b4.Location = new System.Drawing.Point(12, 118);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(100, 100);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b8.Location = new System.Drawing.Point(118, 224);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(100, 100);
+            this.b8.TabIndex = 8;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b9.Location = new System.Drawing.Point(224, 224);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(100, 100);
+            this.b9.TabIndex = 7;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.b7.Location = new System.Drawing.Point(12, 224);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(100, 100);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.Toplu_Tık);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 330);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X - O - X";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b7;
+    }
+}
+
---
+++ X - O - X/X - O - X/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X___O___X
+{
+    public partial class Form1 : Form
+    {
+        bool X_Sıra;
+        bool O_Sıra;
+
+        int işlem_Sayısı = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void X_Belirle()
+        {
+            //yatay başlangıç 
+
+            if (b1.Text == "X" && b2.Text == "X" && b3.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+            if (b3.Text == "X" && b5.Text == "X" && b6.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+            if (b7.Text == "X" && b8.Text == "X" && b9.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+            // YATAY BİTİŞ
+
+            // Dikey Başlangıç
+
+            if (b1.Text == "X" && b4.Text == "X" && b7.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+            if (b2.Text == "X" && b5.Text == "X" && b8.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+            if (b3.Text == "X" && b6.Text == "X" && b9.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+            // Dikey bitiş 
+
+            // Çapraz Başlangıç
+
+            if (b1.Text == "X" && b5.Text == "X" && b9.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+            if (b3.Text == "X" && b5.Text == "X" && b7.Text == "X")
+            {
+                Temizle();
+                MessageBox.Show("X kazandı,", "Tebrikler, X!");
+            }
+
+
+            // çapraz bitiş
+        }
+
+
+
+        public void O_Belirle()
+        {
+            //yatay başlangıç 
+
+            if (b1.Text == "O" && b2.Text == "O" && b3.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+            if (b3.Text == "O" && b5.Text == "O" && b6.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+            if (b7.Text == "O" && b8.Text == "O" && b9.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+            // YATAY BİTİŞ
+
+            // Dikey Başlangıç
+
+            if (b1.Text == "O" && b4.Text == "O" && b7.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+            if (b2.Text == "O" && b5.Text == "O" && b8.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+            if (b3.Text == "O" && b6.Text == "O" && b9.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+            // Dikey bitiş 
+
+            // Çapraz Başlangıç
+
+            if (b1.Text == "O" && b5.Text == "O" && b9.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+            if (b3.Text == "O" && b5.Text == "O" && b7.Text == "O")
+            {
+                Temizle();
+                MessageBox.Show("O kazandı,", "Tebrikler, O!");
+            }
+
+
+            // çapraz bitiş
+        }
+
+        public void Temizle()
+        {
+            b1.Text = "";
+            b2.Text = "";
+            b3.Text = "";
+            b4.Text = "";
+            b5.Text = "";
+            b6.Text = "";
+            b7.Text = "";
+            b8.Text = "";
+            b9.Text = "";
+
+
+            b1.Enabled = true;
+            b2.Enabled = true;
+            b3.Enabled = true;
+            b4.Enabled = true;
+            b5.Enabled = true;
+            b6.Enabled = true;
+            b7.Enabled = true;
+            b8.Enabled = true;
+            b9.Enabled = true;
+
+            işlem_Sayısı = 0;
+        }
+
+
+        public void Beraberlik()
+        {
+            if(işlem_Sayısı == 9)
+            {
+                Temizle();
+                MessageBox.Show("Berabere bitti", "beraberlik...");
+            }
+
+        }
+
+        private void Toplu_Tık(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            işlem_Sayısı = işlem_Sayısı + 1;
+
+            if(X_Sıra)
+            {
+                X_Sıra = false;
+                O_Sıra = true;
+
+                b.Text = "X";
+                b.Enabled = false;
+
+                X_Belirle();
+                Beraberlik();
+
+            }
+
+            else if (O_Sıra)
+            {
+                X_Sıra = true;
+                O_Sıra = false;
+
+                b.Text = "0";
+                b.Enabled = false;
+
+                O_Belirle();
+                Beraberlik();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X_Sıra = true;
+        }
+    }
+}
+
+
