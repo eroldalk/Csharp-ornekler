87be97e2a588caf3d861c5b4b1ad022e103d15f5
Erol Dalkılıç <eroldalg@gmail.com>
2022-04-12 00:06:41 +0300

    arrows

---
+++ Araba Yarışı 2D/Araba Yarışı 2D/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Araba_Yarışı_2D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelSolSerit1 = new System.Windows.Forms.Label();
+            this.labelSagSerit1 = new System.Windows.Forms.Label();
+            this.labelSolSerit3 = new System.Windows.Forms.Label();
+            this.labelSagSerit4 = new System.Windows.Forms.Label();
+            this.labelSolSerit4 = new System.Windows.Forms.Label();
+            this.labelSagSerit3 = new System.Windows.Forms.Label();
+            this.labelSolSerit2 = new System.Windows.Forms.Label();
+            this.labelSagSerit2 = new System.Windows.Forms.Label();
+            this.labelSolSerit5 = new System.Windows.Forms.Label();
+            this.labelSagSerit5 = new System.Windows.Forms.Label();
+            this.RedCar = new System.Windows.Forms.PictureBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.labelRoad = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RedCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(0, -7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 658);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(668, -7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 680);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(0, 636);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(694, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // labelSolSerit1
+            // 
+            this.labelSolSerit1.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit1.Location = new System.Drawing.Point(216, 535);
+            this.labelSolSerit1.Name = "labelSolSerit1";
+            this.labelSolSerit1.Size = new System.Drawing.Size(11, 82);
+            this.labelSolSerit1.TabIndex = 10;
+            this.labelSolSerit1.Text = "\r\n";
+            // 
+            // labelSagSerit1
+            // 
+            this.labelSagSerit1.BackColor = System.Drawing.Color.White;
+            this.labelSagSerit1.Location = new System.Drawing.Point(444, 535);
+            this.labelSagSerit1.Name = "labelSagSerit1";
+            this.labelSagSerit1.Size = new System.Drawing.Size(11, 79);
+            this.labelSagSerit1.TabIndex = 9;
+            this.labelSagSerit1.Text = "\r\n";
+            // 
+            // labelSolSerit3
+            // 
+            this.labelSolSerit3.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit3.Location = new System.Drawing.Point(216, 282);
+            this.labelSolSerit3.Name = "labelSolSerit3";
+            this.labelSolSerit3.Size = new System.Drawing.Size(11, 82);
+            this.labelSolSerit3.TabIndex = 12;
+            this.labelSolSerit3.Text = "\r\n";
+            // 
+            // labelSagSerit4
+            // 
+            this.labelSagSerit4.BackColor = System.Drawing.Color.White;
+            this.labelSagSerit4.Location = new System.Drawing.Point(444, 160);
+            this.labelSagSerit4.Name = "labelSagSerit4";
+            this.labelSagSerit4.Size = new System.Drawing.Size(11, 79);
+            this.labelSagSerit4.TabIndex = 11;
+            this.labelSagSerit4.Text = "\r\n";
+            // 
+            // labelSolSerit4
+            // 
+            this.labelSolSerit4.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit4.Location = new System.Drawing.Point(216, 157);
+            this.labelSolSerit4.Name = "labelSolSerit4";
+            this.labelSolSerit4.Size = new System.Drawing.Size(11, 82);
+            this.labelSolSerit4.TabIndex = 14;
+            this.labelSolSerit4.Text = "\r\n";
+            // 
+            // labelSagSerit3
+            // 
+            this.labelSagSerit3.BackColor = System.Drawing.Color.White;
+            this.labelSagSerit3.Location = new System.Drawing.Point(444, 285);
+            this.labelSagSerit3.Name = "labelSagSerit3";
+            this.labelSagSerit3.Size = new System.Drawing.Size(11, 79);
+            this.labelSagSerit3.TabIndex = 13;
+            this.labelSagSerit3.Text = "\r\n";
+            // 
+            // labelSolSerit2
+            // 
+            this.labelSolSerit2.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit2.Location = new System.Drawing.Point(216, 416);
+            this.labelSolSerit2.Name = "labelSolSerit2";
+            this.labelSolSerit2.Size = new System.Drawing.Size(11, 82);
+            this.labelSolSerit2.TabIndex = 16;
+            this.labelSolSerit2.Text = "\r\n";
+            // 
+            // labelSagSerit2
+            // 
+            this.labelSagSerit2.BackColor = System.Drawing.Color.White;
+            this.labelSagSerit2.Location = new System.Drawing.Point(444, 416);
+            this.labelSagSerit2.Name = "labelSagSerit2";
+            this.labelSagSerit2.Size = new System.Drawing.Size(11, 79);
+            this.labelSagSerit2.TabIndex = 15;
+            this.labelSagSerit2.Text = "\r\n";
+            // 
+            // labelSolSerit5
+            // 
+            this.labelSolSerit5.BackColor = System.Drawing.Color.White;
+            this.labelSolSerit5.Location = new System.Drawing.Point(216, 26);
+            this.labelSolSerit5.Name = "labelSolSerit5";
+            this.labelSolSerit5.Size = new System.Drawing.Size(11, 82);
+            this.labelSolSerit5.TabIndex = 18;
+            this.labelSolSerit5.Text = "\r\n";
+            // 
+            // labelSagSerit5
+            // 
+            this.labelSagSerit5.BackColor = System.Drawing.Color.White;
+            this.labelSagSerit5.Location = new System.Drawing.Point(444, 26);
+            this.labelSagSerit5.Name = "labelSagSerit5";
+            this.labelSagSerit5.Size = new System.Drawing.Size(11, 79);
+            this.labelSagSerit5.TabIndex = 17;
+            this.labelSagSerit5.Text = "\r\n";
+            // 
+            // RedCar
+            // 
+            this.RedCar.Image = ((System.Drawing.Image)(resources.GetObject("RedCar.Image")));
+            this.RedCar.Location = new System.Drawing.Point(282, 343);
+            this.RedCar.Name = "RedCar";
+            this.RedCar.Size = new System.Drawing.Size(113, 194);
+            this.RedCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RedCar.TabIndex = 19;
+            this.RedCar.TabStop = false;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Black;
+            this.label14.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(31, 638);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(91, 30);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "Speed :";
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.BackColor = System.Drawing.Color.Black;
+            this.labelSpeed.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSpeed.ForeColor = System.Drawing.Color.White;
+            this.labelSpeed.Location = new System.Drawing.Point(116, 638);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(91, 30);
+            this.labelSpeed.TabIndex = 21;
+            this.labelSpeed.Text = "0 km/h";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.Black;
+            this.label16.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.Color.White;
+            this.label16.Location = new System.Drawing.Point(239, 638);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(106, 38);
+            this.label16.TabIndex = 22;
+            this.label16.Text = "Road :";
+            // 
+            // labelRoad
+            // 
+            this.labelRoad.AutoSize = true;
+            this.labelRoad.BackColor = System.Drawing.Color.Black;
+            this.labelRoad.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRoad.ForeColor = System.Drawing.Color.White;
+            this.labelRoad.Location = new System.Drawing.Point(327, 638);
+            this.labelRoad.Name = "labelRoad";
+            this.labelRoad.Size = new System.Drawing.Size(68, 38);
+            this.labelRoad.TabIndex = 23;
+            this.labelRoad.Text = "0 m";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.Color.Black;
+            this.label18.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.ForeColor = System.Drawing.Color.White;
+            this.label18.Location = new System.Drawing.Point(424, 636);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(193, 38);
+            this.label18.TabIndex = 24;
+            this.label18.Text = "High Score :";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.BackColor = System.Drawing.Color.Black;
+            this.labelScore.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.ForeColor = System.Drawing.Color.White;
+            this.labelScore.Location = new System.Drawing.Point(575, 636);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(54, 30);
+            this.labelScore.TabIndex = 25;
+            this.labelScore.Text = "0 m";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(706, 672);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.labelRoad);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.RedCar);
+            this.Controls.Add(this.labelSolSerit5);
+            this.Controls.Add(this.labelSagSerit5);
+            this.Controls.Add(this.labelSolSerit2);
+            this.Controls.Add(this.labelSagSerit2);
+            this.Controls.Add(this.labelSolSerit4);
+            this.Controls.Add(this.labelSagSerit3);
+            this.Controls.Add(this.labelSolSerit3);
+            this.Controls.Add(this.labelSagSerit4);
+            this.Controls.Add(this.labelSolSerit1);
+            this.Controls.Add(this.labelSagSerit1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Araba Yarışı 2D";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.RedCar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelSolSerit1;
+        private System.Windows.Forms.Label labelSagSerit1;
+        private System.Windows.Forms.Label labelSolSerit3;
+        private System.Windows.Forms.Label labelSagSerit4;
+        private System.Windows.Forms.Label labelSolSerit4;
+        private System.Windows.Forms.Label labelSagSerit3;
+        private System.Windows.Forms.Label labelSolSerit2;
+        private System.Windows.Forms.Label labelSagSerit2;
+        private System.Windows.Forms.Label labelSolSerit5;
+        private System.Windows.Forms.Label labelSagSerit5;
+        private System.Windows.Forms.PictureBox RedCar;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label labelRoad;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label labelScore;
+    }
+}
+
---
+++ Araba Yarışı 2D/Araba Yarışı 2D/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Araba_Yarışı_2D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int SeritSayisi = 1;
+
+        private void AracYerine()
+        {
+            if(SeritSayisi == 1)
+            {
+                RedCar.Location = new Point(200, 343);
+            }
+            else if (SeritSayisi == 0)
+            {
+                RedCar.Location = new Point(60, 343);
+            }
+            else if (SeritSayisi == 2)
+            {
+                RedCar.Location = new Point(400, 343);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                if (SeritSayisi < 2) 
+                    SeritSayisi++;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if(SeritSayisi > 0)
+                    SeritSayisi--;
+            }
+            AracYerine();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AracYerine();
+        }
+       
+    }
+}
